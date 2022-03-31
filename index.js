70166c1df4e85cb24c563f4040038a9714656d56
Brayam1993 <argofrog@live.com.mx>
2022-03-31 10:23:57 -0600

    import module to index

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const y1 = 1;
 const y2 = 1;
 
+const math = require('./math.js');
 
-
-console.log('add:',add(y1,y2));
-console.log('sub:',substract(y1,y2));
-console.log('mul:',multiply(y1,y2));
-console.log('div:',divide(y1,y2));
+console.log('add:',math.add(y1,y2));
+console.log('sub:',math.substract(y1,y2));
+console.log('mul:',math.multiply(y1,y2));
+console.log('div:',math.divide(y1,y2));
