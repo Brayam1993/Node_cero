16d8466c00afc2a148d9089ba8ef5e490ff50fd0
Brayam1993 <argofrog@live.com.mx>
2022-03-31 11:39:09 -0600

    code async vs blocked

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-const os = require('os'); // llamando modulo os de node
+// 1. Se require modulo file sistem
+// 2. Se guarda en constante con el mismo nombre del modulo
+// 2.1 dos metodos principales del modulo se usa 
+//     para poder crear archivos nuevos y poder leerlos
 
-console.log(os.platform()); // Se muestra por consola el metodo platform
-console.log(os.release());
-console.log('fee mem:',os.freemem(),'bytes');
-console.log('Tot mem:',os.totalmem(),'bytes');
+const fs = require('fs'); 
+
+//  Se empieza creando un nuevo archivo
+//  Desde -*-modulo-*- fs se llama al metodo writefile
+//  El -*-metodo-*- write file es para poder escribir un archivo
+//  El metodo write file recibe el -*-parametro-*- nombre del 
+//      archivo que se llama texto.tx y recibe la direccion de 
+//      donde se quiere crear. 
+//      Como se crea en el mismo proyecto se coloca ./
+//  Como segundo parametro se pasa el contenido.
+//  Recibe un parametro mas, un callback -*-* Funcion 
+//      que se ejecuta cuando ha terminado el proceso anterior
+//  La funcion recibe el parametro err
+//  Si el error existe se manda por consola un mesaje mostrando
+//      el error
+//  Si no, se manda por consola un mensaje "Archivo creado"
+
+fs.writeFile('./texto.txt','linea uno',function(err){
+    if(err) {
+        console.log(err);
+    }
+    console.log('Archivo creado');
+});
+
+//  La funcion se conoce como callback
+//  El codigo se conoce como codigo asincrono  
+
+// const result = fs.writefile('', ''); // Codigo bloqueante
+
+console.log('ultima linea de codigo');
