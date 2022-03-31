47505a7151fd888f715393805115c4b8b185ed6b
Brayam1993 <argofrog@live.com.mx>
2022-03-31 10:15:06 -0600

    math function

---
+++ index.js
@@ -0,0 +1,9 @@
+const y1 = 1;
+const y2 = 1;
+
+
+
+console.log('add:',add(y1,y2));
+console.log('sub:',substract(y1,y2));
+console.log('mul:',multiply(y1,y2));
+console.log('div:',divide(y1,y2));
---
+++ math.js
@@ -0,0 +1,19 @@
+function add(x1, x2) {
+    return x1+ x2;
+}
+
+function substract(x1, x2) {
+    return x1 - x2;
+}
+
+function multiply(x1, x2) {
+    return x1*x2;
+}
+
+function divide(x1, x2) {
+    if(x1 === 0 || x2 === 0 ) {
+        console.log('no se puede dividir por cero 0')
+    }else{
+        return x1/x2;
+    }
+}
