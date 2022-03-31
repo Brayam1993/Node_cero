bff5fbd4989341a263d74bd85df11f6e8609b607
Brayam1993 <argofrog@live.com.mx>
2022-03-31 10:36:28 -0600

    Show what maths import in indes file

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const y2 = 0;
 
 const math = require('./math.js');
 
+console.log(math);
+/*
 console.log('add:',math.add(y1,y2));
 console.log('sub:',math.substract(y1,y2));
 console.log('mul:',math.multiply(y1,y2));
 console.log('div:',math.divide(y1,y2));
+*/
